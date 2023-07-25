0b6c11833ba3fac50958a9cfa0f3e6d96c44e57d
Lana Cazandra Legaspi <lanacazandra@gmail.com>
2023-07-25 23:18:02 +0800

    trial: nageexperiment pa rin po

--- main.py
+++ main.py
@@ -14,12 +14,14 @@ root.geometry ("850x500")
 # GIVING TITLE TO THE WINDOW
 root.title ("COVID-19 CONTACT TRACING APP")
 
-# MAKING A LABEL
-title_label = tk.Label(root, text="COVID-19 CONTACT TRACING APP", font= ("Times New Roman", 18))
+# MAKING LABEL
+title_label = tk.Label(root, text="COVID-19 CONTACT TRACING APP", font= ("Times New Roman", 18, 'bold'))
 title_label.pack(padx=20)
 
-instruction_label = tk.Label(root, text = "NOTE: Please fill up the details needed for contact tracing", font= ("Cambria", 12))
+instruction_label = tk.Label(root, text = "NOTE: Please fill up the details needed for contact tracing", font= ("Cambria", 12, 'italic'))
 instruction_label.pack()
+
+# MAKING ENTRY TEXTBOX FOR RESPONDENT'S PERSONAL INFORMATION
 
 
 root.mainloop()
